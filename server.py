253a0ca74ed5cda89009564132701e31b72b45ff
DrSparky-2007 <56183042+DrSparky-2007@users.noreply.github.com>
2020-09-05 19:14:11 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,9 @@
+import socket
+import threading
+
+HEADER = 16
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
