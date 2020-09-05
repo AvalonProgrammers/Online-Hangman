1b5009c3e9f0081e897a3e3c6d91f564bd53f5a4
DrSparky-2007 <56183042+DrSparky-2007@users.noreply.github.com>
2020-09-05 19:23:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,12 +6,18 @@ PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = "!DisconnectUser"
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
 
 def handle_user(conn,addr):
     pass
 
 def start():
-    pass
+    server.listen()
+    print("[STATUS] Listening")
+    while True:
+        pass
 
 start()
