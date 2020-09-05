a2d4e77b0081c837c07e567b64575770ae3cfd1f
DrSparky-2007 <56183042+DrSparky-2007@users.noreply.github.com>
2020-09-05 19:19:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,3 +7,11 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+
+def handle_user(conn,addr):
+    pass
+
+def start():
+    pass
+
+start()
